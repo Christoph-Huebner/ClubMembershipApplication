1c89ae8757db106041a9829b6a9e97e3b4199a2b
huebner.christoph <huebner.christoph@t-online.de>
2023-07-13 20:34:34 +0200

    Add project files.

---
+++ ClubMembershipApplication/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string AddressFirstLine { get; set; } = string.Empty;
+        public string AddressSecondLine { get; set;} = string.Empty;
+        public string AddressCity { get; set;} = string.Empty;
+        public string PostCode { get; set;} = string.Empty;
+    }
+}
