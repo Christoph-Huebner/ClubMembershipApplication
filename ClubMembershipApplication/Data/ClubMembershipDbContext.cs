d24f0b4f8b720cdfd39995c017d978b0b668cc4b
huebner.christoph <huebner.christoph@t-online.de>
2023-07-13 20:59:43 +0200

    Make some changes on the ClubMembershipDbContext class (connection string, configuration). Do a migration to the database. Before that install the Microsoft.EntityFrameworkCore.Tools as well.

--- ClubMembershipApplication/Data/ClubMembershipDbContext.cs
+++ ClubMembershipApplication/Data/ClubMembershipDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClubMembershipApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace ClubMembershipApplication.Data
 {
-    internal class ClubMembershipDbContext
+    public class ClubMembershipDbContext : DbContext 
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembership.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        // Run Add-Migration FirstCreate on the Package Manager Console
+        // Hint: the 'Microsoft.EntityFrameworkCore.Sqlite' & 'Microsoft.EntityFrameworkCore.Tools' are needed
+        public DbSet<User> User { get; set; }
     }
 }
