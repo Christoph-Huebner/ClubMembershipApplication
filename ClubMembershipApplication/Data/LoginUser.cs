22618f83f1d827a1440efbda3f28b09dc7e99911
huebner.christoph <huebner.christoph@t-online.de>
2023-07-14 22:39:49 +0200

    Implement the ILogin interface with the LoginUser class (also file created). Make a datebase querry for check to credentials. Return a null User object if something is wrong.

---
+++ ClubMembershipApplication/Data/LoginUser.cs
@@ -0,0 +1,22 @@
+﻿using ClubMembershipApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Data
+{
+    public class LoginUser : ILogin
+    {
+        public User Login(string eMailAddress, string password)
+        {
+            User user = null;
+            using (var dbContext = new ClubMembershipDbContext())
+            {
+                user = dbContext.User.FirstOrDefault(u => u.EMailAdress.Trim().ToLower() == eMailAddress.Trim().ToLower() && u.Password.Equals(password));
+            }
+            return user;
+        }
+    }
+}
