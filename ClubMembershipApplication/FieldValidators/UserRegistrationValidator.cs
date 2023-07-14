8b047c36b88afe7b303a4ce155d0f0956e732f87
huebner.christoph <huebner.christoph@t-online.de>
2023-07-14 16:56:58 +0200

    Use the interface IFieldValidator in the UserRegistrationValidator class. Implement the FieldArray, ValidatorDel member variables and the method InitialiseValidatorDelegates. The last one uses the delegates from the FieldValidatorAPI by call the properties.

--- ClubMembershipApplication/FieldValidators/UserRegistrationValidator.cs
+++ ClubMembershipApplication/FieldValidators/UserRegistrationValidator.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ClubMembershipApplication.FieldValidators
 {
-    public class UserRegistrationValidator
+    public class UserRegistrationValidator : IFieldValidator
     {
         const int FirstName_Min_Length = 2;
         const int FirstName_Max_Length = 100;
@@ -15,8 +15,7 @@ namespace ClubMembershipApplication.FieldValidators
         const int LastName_Max_Length = 100;
 
         delegate bool EmailExistsDel (string emailAddress);
-
-        FieldValidatorDel? _fieldValidatorDel = null;
+        EmailExistsDel? _emailExistsDel = null;
 
         RequiredValidDel? _requiredValidDel = null;
         StringLengthValidDel? _stringLengthValidDel = null;
@@ -24,5 +23,28 @@ namespace ClubMembershipApplication.FieldValidators
         PatternMatchDel? _patternMatchDel = null;
         CompareFieldsValidDel? _compareFieldsValidDel = null;
 
+        string[]? _fieldArray = null;
+        public string[] FieldArray
+        {
+            get
+            {
+                if (_fieldArray == null)
+                    _fieldArray = new string[Enum.GetValues(typeof(FieldConstants.UserRegistrationField)).Length];
+                
+                return _fieldArray;   
+            }
+        }
+
+        FieldValidatorDel? _fieldValidatorDel = null;
+        public FieldValidatorDel ValidatorDel => _fieldValidatorDel;
+
+        public void InitialiseValidatorDelegates()
+        {
+            _requiredValidDel = CommonFieldValidatorFunctions.RequiredFieldValidDel;
+            _stringLengthValidDel = CommonFieldValidatorFunctions.StringLengthValidDel;
+            _dateValidDel = CommonFieldValidatorFunctions.DateValidDel;
+            _patternMatchDel = CommonFieldValidatorFunctions.PatternMatchDel;
+            _compareFieldsValidDel = CommonFieldValidatorFunctions.CompareFieldsValidDel;
+        }
     }
 }
