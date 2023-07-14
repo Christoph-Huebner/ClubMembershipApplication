fd958cacbd5d571a8571a16579244452597bfc28
huebner.christoph <huebner.christoph@t-online.de>
2023-07-14 22:29:40 +0200

    Create the class Factory (also file).

---
+++ ClubMembershipApplication/Factory.cs
@@ -0,0 +1,20 @@
+﻿using ClubMembershipApplication.Data;
+using ClubMembershipApplication.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication
+{
+    public static class Factory
+    {
+        public static IView GetMainViewObject()
+        {
+            // ToDo: Implementation of Login
+            IView view = null;
+            return view;
+        }
+    }
+}
