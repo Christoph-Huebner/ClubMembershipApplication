6f55e216bcc683a0a0bcab9197a98030ba961483
huebner.christoph <huebner.christoph@t-online.de>
2023-07-14 16:36:02 +0200

    Create the class UserRegistrationValidator (also file) and defines min, max values for the firstname and lastname. Link the FieldValidatorAPI class library the main project. Create 5 delegate member variables from this project. Also create a delegate for EMail validation in the UserRegistrationValidator class.

---
+++ ClubMembershipApplication/FieldValidators/UserRegistrationValidator.cs
@@ -0,0 +1,28 @@
+﻿using FieldValidatorAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.FieldValidators
+{
+    public class UserRegistrationValidator
+    {
+        const int FirstName_Min_Length = 2;
+        const int FirstName_Max_Length = 100;
+        const int LastName_Min_Length = 2;
+        const int LastName_Max_Length = 100;
+
+        delegate bool EmailExistsDel (string emailAddress);
+
+        FieldValidatorDel? _fieldValidatorDel = null;
+
+        RequiredValidDel? _requiredValidDel = null;
+        StringLengthValidDel? _stringLengthValidDel = null;
+        DateValidDel? _dateValidDel = null;
+        PatternMatchDel? _patternMatchDel = null;
+        CompareFieldsValidDel? _compareFieldsValidDel = null;
+
+    }
+}
