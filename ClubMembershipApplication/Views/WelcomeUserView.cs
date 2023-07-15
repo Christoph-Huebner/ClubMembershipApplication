03dd401d474362298c990733102662fa4235b61f
huebner.christoph <huebner.christoph@t-online.de>
2023-07-15 18:53:39 +0200

    Add the clear console and WriteMainHeading cmd in the RunAll method of the WelcomeUserView class.

--- ClubMembershipApplication/Views/WelcomeUserView.cs
+++ ClubMembershipApplication/Views/WelcomeUserView.cs
@@ -21,6 +21,8 @@ namespace ClubMembershipApplication.Views
 
         public void RunView()
         {
+            Console.Clear();
+            CommonOutputText.WriteMainHeading();
             CommonOutputFormat.ChangeFontColor(FontTheme.Success);
             Console.WriteLine($"Hi {_user.FirstName}!!{Environment.NewLine}Welcome to the Cycling club!!");
             CommonOutputFormat.ChangeFontColor(FontTheme.Default);
